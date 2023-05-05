38d26ca86de175ef3103ffb8bbe862372fa5fcd0
samv1121 <93051118+samv1121@users.noreply.github.com>
2023-05-05 16:44:28 +0530

    Add files via upload

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      deepPurple: '#682cbf',
+      leagueChampion: '#d40c00',
+      leagueIndigo: '#66ADF2',
+      leagueBLue: '#526eff',
+      leagueGolden: '#ffd700',
+      leagueSilver: '#c0c0c0',
+      leagueBronze: '#CD7F32',
+      leagueSecondary: '#696969',
+      darkbg: '#292829',
+      whttxt: '#dadada',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
